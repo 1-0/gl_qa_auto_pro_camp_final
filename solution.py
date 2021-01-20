4e5bf625b6e0a43c663cf8b402a3e58013de65ef
1-0 <1_0@usa.com>
2021-01-20 17:13:53 +0200

    Added connection to linux a and tracepath linux b

--- solution.py
+++ solution.py
@@ -3,18 +3,42 @@
 import sys
 import os
 from os.path import join, dirname
+import subprocess
 import paramiko
 from tools.params import load_params
 
 
-def trace_log(path):
+def connect_a(hostname, port, username, password, command='ls -l'):
+    with paramiko.SSHClient() as client:
+        client.load_system_host_keys()
+        client.connect(
+            hostname=hostname,
+            port=port,
+            username=username,
+            password=password
+        )
+        stdin, stdout, stderr = client.exec_command(command)
+        print(stdout.read().decode())
+        return stdout.read().decode()[:]
+
+
+
+def trace_log(url):
+    # return subprocess.check_output(['/usr/bin/tracepath', url]).decode()
     pass
 
 
 def main():
     p = load_params()
-    print(p)
-    trace_log(p)
+    # print(p)
+    # print(trace_log(p["linux2"]))
+    connect_a(
+        hostname=p["linux1"], 
+        port=p["port1"], 
+        username=p["user1"], 
+        password=p["password1"],
+        command='/usr/bin/tracepath ' + p["linux2"]
+        )
 
 
 if __name__ == '__main__':
--- tools/params.py
+++ tools/params.py
@@ -9,9 +9,7 @@ The params module load and return param from:
 True
 >>> load_param(a, "test_file", str, "test_file.bin")
 'test_file.bin'
->>> load_param(a, "test_file", str)
->>> load_params()
-{'test_file': None, 'test_file_size': 100.0, 'linux2': 'Linux-B', 'port2': 22}
+>>> load_param(a, "test_file", str, None)
 """
 
 
@@ -22,12 +20,13 @@ from dotenv import load_dotenv
 
 def load_param(args, param_name, param_type, default=None):
     """load_param - load and return param from args or .env or default"""
-    try:
-        if getattr(args, param_name.lower()):
+    try:  # for python38 and python39
+        if getattr(args, param_name.lower()) is not None:
             return getattr(args, param_name.lower())
     except AttributeError:
-        if os.getenv(param_name.upper()):
-            return param_type(os.getenv(param_name.upper()))
+        pass
+    if os.getenv(param_name.upper()):
+        return param_type(os.getenv(param_name.upper()))
     return default
 
 
@@ -51,21 +50,37 @@ def load_params():
               '(if no test_file) for testing connection speed')
     )
     parser.add_argument(
+        '-l1', '--linux1', required=False,
+        metavar='linux1', type=str,
+        help='linux A machine address'
+    )
+    parser.add_argument(
+        '-p1', '--port1', required=False,
+        metavar='port1', type=int,
+        help='linux A machine ssh connection port'
+    )
+    parser.add_argument(
         '-l2', '--linux2', required=False,
         metavar='linux2', type=str,
-        help='linux 2 machine address'
+        help='linux B machine address'
     )
     parser.add_argument(
         '-p2', '--port2', required=False,
         metavar='port2', type=int,
-        help='linux 2 machine ssh connection port'
+        help='linux B machine ssh connection port'
     )
 
     args = parser.parse_args()
     res["test_file"] = load_param(args, "test_file", str)
     res["test_file_size"] = load_param(args, "test_file_size", float, 100.)
+    res["linux1"] = load_param(args, "linux1", str, "Linux-A")
+    res["port1"] = load_param(args, "port1", int, 22)
+    res["user1"] = load_param(args, "user1", str, "ubuntu")
+    res["password1"] = load_param(args, "password1", str)
     res["linux2"] = load_param(args, "linux2", str, "Linux-B")
     res["port2"] = load_param(args, "port2", int, 22)
+    res["user2"] = load_param(args, "user2", str, "ubuntu")
+    res["password2"] = load_param(args, "password2", str)
     return res
 
 
